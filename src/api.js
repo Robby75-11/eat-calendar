e637315813f126e1e46f466c15b7f9179e3ac25a
Roberto Albergo <ralbergo7@gmail.com>
2025-08-25 15:37:56 +0200

    env

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // 🔧 cambia se deployi in prod
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // 🔧 cambia se deployi in prod
 });
 
 // Interceptor per aggiungere automaticamente il token a ogni richiesta
